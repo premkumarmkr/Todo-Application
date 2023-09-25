ea0ed81c3012e30cc091c9207ddd810867359fe8
premkumarmkr <35822992+premkumarmkr@users.noreply.github.com>
2023-09-25 06:44:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,12 @@
-#Just adding this line to test the triger.
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask
+import requests
+# If `entrypoint` is not defined in app.yaml, GKE will look for an app
+# called `app` in `main.py`.
 app = Flask(__name__)
-
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-
-
-@app.route("/")
-def home():
-    todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
-
-
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("home"))
-
-
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("home"))
-
-
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("home"))
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True,host="0.0.0.0")
+@app.route('/', methods=['GET'])
+def index():
+    """Return the data from our nginx deployed server"""
+	return "Welcome to Premkumar World"
+if __name__ == '__main__':
+    # We will now run our application
+    app.run(host='localhost', port=8080, debug=True)
